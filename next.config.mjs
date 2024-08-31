c7930b977ae0b373ad4504005afb23196457e1ae
Akhatar Mourad <100293325+akhatarmourad@users.noreply.github.com>
2024-08-31 09:56:48 +0100

    Edit nextjs config to solve probs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 import {withSentryConfig} from '@sentry/nextjs';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: "standalone",
 };
 
 export default withSentryConfig(nextConfig, {
